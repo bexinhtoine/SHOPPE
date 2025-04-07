88dab1dc6796b37f7973bb284895f71d9819d529
Guen Nhat King <guennhatking@gmail.com>
2025-04-07 17:28:20 +0700

    Lưu trước khi pull

---
+++ ShoppeWebApp/Controllers/AllProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppeWebApp.Controllers
+{
+    public class AllProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
