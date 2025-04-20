2e5fdaf44c71532a1e736e474c51dc433d611114
LuanNguyen23T <nguyenhuuhoangluan194@gmail.com>
2025-04-20 21:33:56 +0700

    Refresh

--- ShoppeWebApp/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShoppeWebApp.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
